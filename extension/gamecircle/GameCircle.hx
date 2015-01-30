8116d1a2f1df1ac8ac50f2d387ccfdb0625d7ef0
sam <sam@silver42.karoo.co.uk>
2015-01-30 17:08:51 +0000

    Fix incorrect package names + better formatting

--- extension/gamecircle/GameCircle.hx
+++ extension/gamecircle/GameCircle.hx
@@ -1,59 +1,48 @@
-package com.samcodes.gamecircle;
+package extension.gamecircle;
 
-import com.samcodes.gamecircle.gc.ConnectionHandler;
-import com.samcodes.gamecircle.gc.GamesClient;
+import extension.gamecircle.gc.ConnectionHandler;
+import extension.gamecircle.gc.GamesClient;
 
 #if android
 import openfl.utils.JNI;
 #end
 
-class GameCircle
-{
-  private var _gamesClient : GamesClient;
+class GameCircle {
+	private var _gamesClient:GamesClient;
 
-  public function new(handler : ConnectionHandler)
-  {
+	public function new(handler:ConnectionHandler) {
+		#if android
+		initJNI();
+		_start(handler);
+		#end
+		_gamesClient = new GamesClient(handler);
+	}
+
+	public var games(get, never):GamesClient;
+	
+	public function get_games():GamesClient {
+		return _gamesClient;
+	}
+
 #if android
-    initJNI();
-    _start(handler);
-#end
+	public function isAvailable():Bool {
+		return _isAvailable();
+	}
 
-    _gamesClient = new GamesClient(handler);
-  }
+	private static function initJNI():Void {
+		if(_start == null) {
+		  _start = JNI.createStaticMethod("com/samcodes/gamecircle/GameCircle", "start", "(Lorg/haxe/lime/HaxeObject;)V");
+		}
+		if(_isAvailable == null) {
+		  _isAvailable = JNI.createStaticMethod("com/samcodes/gamecircle/GameCircle", "isAvailable", "()Z");
+		}
+	}
 
-  public var games(get, never) : GamesClient;
-  public function get_games() : GamesClient
-  {
-    return _gamesClient;
-  }
-  
-#if android
-  public function isAvailable() : Bool
-  {
-    return _isAvailable();
-  }
-
-  private static function initJNI()
-  {
-    if(_start == null)
-    {
-      _start = JNI.createStaticMethod("com/samcodes/gamecircle/GameCircle",
-          "start", "(Lorg/haxe/lime/HaxeObject;)V");
-    }
-
-    if(_isAvailable == null)
-    {
-      _isAvailable = JNI.createStaticMethod("com/samcodes/gamecircle/GameCircle",
-          "isAvailable", "()Z");
-    }
-  }
-
-  private static var _start : Dynamic = null;
-  private static var _isAvailable : Dynamic = null;
+	private static var _start:Dynamic = null;
+	private static var _isAvailable:Dynamic = null;
 #else
-  public function isAvailable() : Bool
-  {
-    return false;
-  }
+	public function isAvailable():Bool {
+		return false;
+	}
 #end
 }
--- extension/gamecircle/gc/ConnectionHandler.hx
+++ extension/gamecircle/gc/ConnectionHandler.hx
@@ -1,28 +1,21 @@
-package com.samcodes.gamecircle.gc;
+package extension.gamecircle.gc;
 
-class ConnectionHandler
-{
-  public function new()
-  {
-  }
+class ConnectionHandler {
+	public function new() {
+	}
 
-  public function onWarning(msg : String, where : String)
-  {
-  }
+	public function onWarning(msg:String, where:String) {
+	}
 
-  public function onError(what : String, code : Int, where : String)
-  {
-  }
+	public function onError(what:String, code:Int, where:String) {
+	}
 
-  public function onException(msg : String, where : String)
-  {
-  }
+	public function onException(msg:String, where:String) {
+	}
 
-  public function onConnectionEstablished(what : String)
-  {
-  }
+	public function onConnectionEstablished(what:String) {
+	}
 
-  public function onSignedOut(what : String)
-  {
-  }
+	public function onSignedOut(what:String) {
+	}
 }
